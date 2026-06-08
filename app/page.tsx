d07c02f6f04bf2ce1872074c67659d9cf8643d25
Rukhsar499 <rukshar.digitalwolf@gmail.com>
2026-06-08 12:25:29 +0530

    hi

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 
 import HeroBanner from "./component/layout/HeroBanner";
 import ServicesSection from "./component/layout/ServicesSection";
+import WhyCeebee from "./component/layout/WhyCeebee";
 
 
 
@@ -8,7 +9,9 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
      <HeroBanner />
+      <WhyCeebee />
      <ServicesSection />
+    
     </div>
   );
 }
